721ed27a1de289e3d24dfd6d3fea1b4ed935389b
Ubuntu <ubuntu@ip-172-31-89-89.ec2.internal>
2019-09-17 11:03:04 +0000

    first commit

---
+++ asserts.py
@@ -0,0 +1,10 @@
+x = 1
+y = 2
+
+"""
+TEST it to fail:
+assert x > y, "{0} should be bigger than {1}".format(x, y)
+"""
+
+print(x + y)
+
---
+++ python.py
@@ -0,0 +1,19 @@
+"""
+def print_message(message):
+    print(message)
+    
+print_message("Hello World")
+"""
+"""
+my_list = [1, 2, 3, 4, 5]
+
+for item in my_list:
+    print(item)
+"""
+"""
+use bash to install flask: sudo pip3 install Flask
+in python.py: from flask import Flask
+
+in bash to  make a list of al the dependencies: pip3 freeze --local > requirements.txt
+"""
+from flask import Flask
